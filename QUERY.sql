187b8da5679d4d40605b91e6f9f6dd6ec8217c84
mehedi <mehedi25122003@gmail.com>
2026-06-15 21:13:00 +0600

    another table added

--- QUERY.sql
+++ QUERY.sql
@@ -37,3 +37,29 @@ CREATE TABLE Matches (
   base_ticket_price INT NOT NULL CHECK (base_ticket_price >= 0),
   match_status match_status_type NOT NULL
 );
+
+-- booking table 
+CREATE TYPE payment_status_type AS enum(
+  'Pending',
+  'Confirmed',
+  'Cancelled',
+  'Refunded'
+);
+
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number varchar(250),
+    payment_status payment_status_type,
+    total_cost INT NOT NULL CHECK (total_cost >= 0),
+  
+
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users(user_id),
+
+
+    CONSTRAINT fk_match FOREIGN KEY (match_id) REFERENCES Matches(match_id),
+
+
+    CONSTRAINT uq_booking UNIQUE (user_id, match_id, seat_number)
+);
